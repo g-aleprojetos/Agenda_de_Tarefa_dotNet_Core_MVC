232a986da8879ceedf5a870772e65a515ee2089f
g-aleprojetos <g.aleprojetos@gmail.com>
2021-04-29 16:21:34 -0300

    atualização do projeto - Banco de dados
    Foi feito o banco de dados

--- Agenda Tarefa/Agenda Tarefa/Models/Tarefa.cs
+++ Agenda Tarefa/Agenda Tarefa/Models/Tarefa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace Agenda_Tarefa.Models
 {
     public class Tarefa
     {
+        public int TarefaId { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "Use menos caracters")]
+        public string Nome { get; set; }
+        
+        public string Data { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [DataType(DataType.Time)]
+        public string Horario { get; set; }
+
     }
 }
