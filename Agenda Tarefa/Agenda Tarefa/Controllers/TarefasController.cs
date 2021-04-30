32b5001307861b1c5a060ae3fe39630f847bdc08
g-aleprojetos <g.aleprojetos@gmail.com>
2021-04-30 10:20:13 -0300

    criado tarefaController

---
+++ Agenda Tarefa/Agenda Tarefa/Controllers/TarefasController.cs
@@ -0,0 +1,29 @@
+﻿using Agenda_Tarefa.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agenda_Tarefa.Controllers
+{
+    public class TarefasController : Controller
+    {
+        private readonly Contexto _contexto;
+
+        public TarefasController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private List<DatasViewModel> PegarDatas()
+        {
+
+        }
+    }
+}
