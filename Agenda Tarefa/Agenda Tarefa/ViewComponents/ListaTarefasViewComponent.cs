a6cd91fd4d32c38f7cefd095af030041c383d913
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-04 13:37:15 -0300

    Criação da ViewComponent

---
+++ Agenda Tarefa/Agenda Tarefa/ViewComponents/ListaTarefasViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Agenda_Tarefa.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agenda_Tarefa.ViewComponents
+{
+    public class ListaTarefasViewComponent : ViewComponent
+    {
+        private readonly Contexto _context;
+
+        public ListaTarefasViewComponent(Contexto contexto)
+        {
+            _context = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string data)
+        {
+            return View(await _context.Tarefas
+                .OrderBy(t => t.Horario).Where(t => t.Data == data).ToListAsync());
+        }
+    }
+}
